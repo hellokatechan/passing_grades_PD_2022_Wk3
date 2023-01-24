078d65c4ef17346cfb4d518cf69f544b9fc72b37
hellokatechan <iamkatechan@gmail.com>
2023-01-23 16:23:47 -0800

    adding commit remotely on Github

--- PD_2022_Wk3.py
+++ PD_2022_Wk3.py
@@ -16,17 +16,17 @@ input2 = input2.rename(columns={"Student ID": "id"})
 #merge input1 and input2 with the id column
 df = pd.merge(input1, input2, on='id',how="inner")
 
+#test if the outcome is performing as expected 
+#newdf = df[(df.id == 1)]
+
 #melt the dataframe 
 df = pd.melt(df,id_vars=["id","gender"],var_name="subject",value_name='score')
 df['pass']= df.score >= 75
-
-#df['average']= df.groupby(['id','gender']).mean()
 
 #create a new df with id,gender, average score, and number of passes)
 df_1 = df.groupby(['id','gender'])['pass'].sum().reset_index(name='number of passed subject')
 df= df.groupby(['id']).mean().round(1)
 df_final = pd.merge(df, df_1, on='id',how='inner').drop(['pass'],axis=1)
 
+# save output on local machin 
 df_final.to_csv('/Users/kt/Desktop/prep/week3_2022/PD_2022_Wk3_output.csv',index=False)
-#newdf = df[(df.id == 1)]
-#print(newdf)
